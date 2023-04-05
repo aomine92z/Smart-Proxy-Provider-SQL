9a667d150a561c9b2b7240a943053a6a9c972f49
AntoineKANG <kzd2001@gmail.com>
2023-04-05 10:35:11 +0200

    insert data

--- src/main/java/createDatasqlite.java
+++ src/main/java/createDatasqlite.java
@@ -2,6 +2,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class createDatasqlite {
 
@@ -25,19 +30,62 @@ public class createDatasqlite {
             stmt.executeUpdate("CREATE TABLE Country(country_name VARCHAR(50) PRIMARY KEY)");
 
             // Create the URL table
-            stmt.executeUpdate("CREATE TABLE URL(Id_URL INTEGER, type INTEGER, country_name VARCHAR(50) NOT NULL, Id_Website INTEGER NOT NULL, PRIMARY KEY(Id_URL), FOREIGN KEY(country_name) REFERENCES Country(country_name), FOREIGN KEY(Id_Website) REFERENCES Website(Id_Website))");
+            stmt.executeUpdate(
+                    "CREATE TABLE URL(Id_URL INTEGER, type INTEGER, country_name VARCHAR(50) NOT NULL, Id_Website INTEGER NOT NULL, PRIMARY KEY(Id_URL), FOREIGN KEY(country_name) REFERENCES Country(country_name), FOREIGN KEY(Id_Website) REFERENCES Website(Id_Website))");
 
             // Create the covers table
-            stmt.executeUpdate("CREATE TABLE covers(Id_Proxy INTEGER, country_name VARCHAR(50), PRIMARY KEY(Id_Proxy, country_name), FOREIGN KEY(Id_Proxy) REFERENCES Proxy(Id_Proxy), FOREIGN KEY(country_name) REFERENCES Country(country_name))");
+            stmt.executeUpdate(
+                    "CREATE TABLE covers(Id_Proxy INTEGER, country_name VARCHAR(50), PRIMARY KEY(Id_Proxy, country_name), FOREIGN KEY(Id_Proxy) REFERENCES Proxy(Id_Proxy), FOREIGN KEY(country_name) REFERENCES Country(country_name))");
 
             // Create the willRespond table
-            stmt.executeUpdate("CREATE TABLE willRespond(Id_URL INTEGER, Id_Proxy INTEGER, triedOk INTEGER, triedTotal INTEGER, PRIMARY KEY(Id_URL, Id_Proxy), FOREIGN KEY(Id_URL) REFERENCES URL(Id_URL), FOREIGN KEY(Id_Proxy) REFERENCES Proxy(Id_Proxy))");
+            stmt.executeUpdate(
+                    "CREATE TABLE willRespond(Id_URL INTEGER, Id_Proxy INTEGER, triedOk INTEGER, triedTotal INTEGER, PRIMARY KEY(Id_URL, Id_Proxy), FOREIGN KEY(Id_URL) REFERENCES URL(Id_URL), FOREIGN KEY(Id_Proxy) REFERENCES Proxy(Id_Proxy))");
 
             // Create the simulation_willRespond table
-            stmt.executeUpdate("CREATE TABLE simulation_willRespond(Id_URL INTEGER, Id_Proxy INTEGER, simulation_probability DOUBLE, PRIMARY KEY(Id_URL, Id_Proxy), FOREIGN KEY(Id_URL) REFERENCES URL(Id_URL), FOREIGN KEY(Id_Proxy) REFERENCES Proxy(Id_Proxy))");
+            stmt.executeUpdate(
+                    "CREATE TABLE simulation_willRespond(Id_URL INTEGER, Id_Proxy INTEGER, simulation_probability DOUBLE, PRIMARY KEY(Id_URL, Id_Proxy), FOREIGN KEY(Id_URL) REFERENCES URL(Id_URL), FOREIGN KEY(Id_Proxy) REFERENCES Proxy(Id_Proxy))");
 
             System.out.println("Database schema created successfully.");
+            // Insert data into the Website table
+            for (int i = 1; i <= 800; i++) {
+                String sql = "INSERT INTO Website (Id_Website) VALUES (?)";
+                try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                    statement.setInt(1, i);
+                    statement.executeUpdate();
+                }
+            }
 
+            // Insert data into the Proxy table
+            for (int i = 1; i <= 60; i++) {
+                String sql = "INSERT INTO Proxy (Id_Proxy, type) VALUES (?, ?)";
+                try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                    statement.setInt(1, i);
+                    int type = (i - 1) % 4 + 1; // This will cycle through the values 1, 2, 3, 4
+                    statement.setInt(2, type);
+                    statement.executeUpdate();
+                }
+            }
+
+            // Insert data into the Country table
+            for (String country : getCountries()) {
+                String sql = "INSERT INTO Country (country_name) VALUES (?)";
+                try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                    statement.setString(1, country);
+                    statement.executeUpdate();
+                }
+            }
+
+            // Insert data into the URL table
+            for (int i = 1; i <= 100000; i++) {
+                String sql = "INSERT INTO URL (Id_URL, type, country_name, Id_Website) VALUES (?, ?, ?, ?)";
+                try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                    statement.setInt(1, i);
+                    statement.setInt(2, (int) (Math.random() * 4) + 1);
+                    statement.setString(3, getCountry());
+                    statement.setInt(4, (int) (Math.random() * 800) + 1);
+                    statement.executeUpdate();
+                }
+            }
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         } finally {
@@ -49,6 +97,86 @@ public class createDatasqlite {
                 e.printStackTrace();
             }
         }
+
+    }
+
+    // private static List<String> getCountries() {
+    // return Arrays.asList("United States", "Canada", "Mexico", "Germany",
+    // "France", "Spain", "Italy",
+    // "United Kingdom", "Russia", "China", "Japan", "South Korea", "India",
+    // "Australia", "Brazil",
+    // "Argentina", "South Africa", "Egypt", "Morocco", "Nigeria");
+    // }
+    private static List<String> getCountries() {
+        return Arrays.asList("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Argentina", "Armenia",
+                "Australia",
+                "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+                "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Botswana", "Brazil", "Brunei", "Bulgaria",
+                "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Chad", "Chile", "China",
+                "Colombia", "Comoros", "Costa Rica", "Cote d\'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic",
+                "Denmark", "Djibouti", "Dominica", "Ecuador", "Egypt", "El Salvador", "Eritrea", "Estonia", "Ethiopia",
+                "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada",
+                "Guatemala",
+                "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia",
+                "Iran",
+                "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati",
+                "Kosovo",
+                "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
+                "Lithuania",
+                "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+                "Mauritania", "Mauritius",
+                "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique",
+                "Myanmar", "Namibia",
+                "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea",
+                "Norway",
+                "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Paraguay", "Peru", "Philippines", "Poland",
+                "Portugal",
+                "Qatar", "Romania", "Russia", "Rwanda", "Saint Lucia", "Samoa", "Saudi Arabia", "Senegal", "Serbia",
+                "Seychelles", "Sierra Leone", "Singapore", "Suriname");
+    }
+
+    private static String getCountry() {
+        List<String> countries = getCountries();
+        Random random = new Random();
+        int index = random.nextInt(countries.size());
+        return countries.get(index);
     }
 
 }
+
+// 'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Argentina',
+// 'Armenia', 'Australia',
+// 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados',
+// 'Belarus', 'Belgium',
+// 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia', 'Botswana', 'Brazil',
+// 'Brunei', 'Bulgaria',
+// 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada',
+// 'Chad', 'Chile',
+// 'China', 'Colombia', 'Comoros', 'Costa Rica', 'Cote d\'Ivoire', 'Croatia',
+// 'Cuba', 'Cyprus',
+// 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Ecuador', 'Egypt', 'El
+// Salvador',
+// 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon',
+// 'Gambia', 'Georgia',
+// 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea',
+// 'Guinea-Bissau', 'Guyana',
+// 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran',
+// 'Iraq', 'Ireland',
+// 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya',
+// 'Kiribati', 'Kosovo',
+// 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia',
+// 'Libya',
+// 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar',
+// 'Malawi', 'Malaysia',
+// 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico',
+// 'Micronesia', 'Moldova',
+// 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar',
+// 'Namibia', 'Nauru',
+// 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria',
+// 'North Korea',
+// 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestine', 'Panama', 'Paraguay',
+// 'Peru',
+// 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda',
+// 'Saint Lucia',
+// 'Samoa', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone',
+// 'Singapore','Suriname'
