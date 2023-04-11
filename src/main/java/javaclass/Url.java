dd59e61167593c65f61663ce5b55014da8c52fc3
Amine <amine.dhane@epfedu.fr>
2023-04-11 18:07:33 +0200

    Last try

--- src/main/java/javaclass/Url.java
+++ src/main/java/javaclass/Url.java
@@ -1,8 +1,11 @@
 package javaclass;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.Connection;
 
 public class URL {
     private int id_URL;
