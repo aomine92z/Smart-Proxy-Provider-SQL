20d7cc2311012c17cd295d57ba09c6b40d3f6d89
Amine <amine.dhane@epfedu.fr>
2023-04-11 18:13:47 +0200

    Last commit

--- src/main/java/javaclass/Url.java
+++ src/main/java/javaclass/Url.java
@@ -1,11 +1,8 @@
 package javaclass;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
 
 public class URL {
     private int id_URL;
